7cd1c448e0c91b39503ba4268100803dbb1fd303
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-06-14 22:23:51 +0200

    feat: now shoot has a rate of fire

---
+++ scripts/constants.gd
@@ -0,0 +1,11 @@
+class_name Literals
+
+class Inputs:
+    const MOVE_UP := "move_up"
+    const MOVE_DOWN := "move_down"
+    const MOVE_RIGHT := "move_right"
+    const MOVE_LEFT := "move_left"
+    const SHOOT := 'shoot'
+class Signals:
+    const SHOOT := 'shoot'
+    const TIMEOUT := 'timeout'
