9979a56fc7683d3c3d960e6ccb9e73974cc93c3e
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-06-17 21:24:37 +0200

    feat: implemented first reload logic

--- scripts/entities/hero/hero.gd
+++ scripts/entities/hero/hero.gd
@@ -11,15 +11,21 @@ extends CharacterBody2D
 @export_range(1000, 5000) var friction: float = 1500
 ## Number of bullets that player is able too shoot within a second.
 @export_range(1, 10) var rate_of_fire: float = 10
-
+## Maximum number of bullets in the magazine_size 
+@export_range(20, 30) var max_magazine_size: int = 20
+## Reload time in seconds
+@export_range(0.1, 2) var reload_time: float = 1.0
 ## Sound effect to be played whenever hero shoots.
 const HERO_SHOOT_SFX = preload ("res://assets/audio/hero_shoot.wav")
 ## Default hero bullet type.
 const BULLET: PackedScene = preload ("res://scenes/entities/Bullet.tscn")
 
 var input: Vector2 = Vector2.ZERO
-var can_shoot: bool = true;
+var can_shoot: bool = true
 var timer: Timer
+
+var magazine_size: int = max_magazine_size
+var is_reloading: bool = false
 
 ## Shooting action to be handled by the Game Manager.
 signal shoot(bullet, direction, location)
@@ -31,13 +37,15 @@ func _ready():
 	timer.connect(Literals.Signals.TIMEOUT, func(): can_shoot=true)
 
 func _process(_delta):
-    if (can_shoot):
-        if (Input.is_action_pressed(Literals.Inputs.SHOOT)):
-            shoot.emit(BULLET, entity.rotation, entity.position, HERO_SHOOT_SFX)
-            can_shoot = false
-            timer.start(1 / rate_of_fire)
-    if (Input.is_action_pressed('quit')):
-        quit.emit()
+	if (can_shoot&&magazine_size != 0):
+		if (Input.is_action_pressed(Literals.Inputs.SHOOT)):
+			_shoot()
+	if (!is_reloading):
+		if (Input.is_action_just_pressed(Literals.Inputs.RELOAD)):
+			_reload()
+
+	if (Input.is_action_pressed('quit')):
+		quit.emit()
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -60,3 +68,16 @@ func player_movement(delta):
 		velocity += (input * accel * delta)
 		velocity = velocity.limit_length(max_speed)
 	move_and_slide()
+
+func _shoot():
+	shoot.emit(BULLET, rotation, position, HERO_SHOOT_SFX)
+	magazine_size = magazine_size - 1
+	can_shoot = false
+	if (magazine_size != 0):
+		timer.start(1 / rate_of_fire)
+
+func _reload():
+	is_reloading = true
+	await get_tree().create_timer(1.0).timeout
+	magazine_size = max_magazine_size
+	is_reloading = false
