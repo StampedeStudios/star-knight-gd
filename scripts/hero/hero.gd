e68d74b31fc38a7ffddab937ef88e24cd11dadbc
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-07-13 19:47:57 +0200

    feat: adjusted movement and size of hero

--- scripts/hero/hero.gd
+++ scripts/hero/hero.gd
@@ -5,11 +5,11 @@ class_name Hero
 ## Movement, shooting and health of player are handled by this script.
 
 ## Maximum speed that should be reachable by the hero.
-@export_range(40, 1000) var max_speed: float = 800
+@export var max_speed: float = 800
 ## Default acceleration of the hero ship.
-@export_range(1000, 5000) var accel: float = 4500
+@export var accel: float = 8000
 ## Default friction of the hero ship.
-@export_range(1000, 5000) var friction: float = 3000
+@export var friction: float = 3000
 ## Number of bullets that player is able too shoot within a second.
 @export_range(1, 10) var rate_of_fire: float = 4
 ## Maximum number of bullets in the ammunition
@@ -19,9 +19,9 @@ class_name Hero
 ## Health
 @export var max_health = 100
 ## Sound effect to be played whenever hero shoots.
-const HERO_SHOOT_SFX = preload("res://assets/audio/hero_shoot.wav")
+const HERO_SHOOT_SFX = preload ("res://assets/audio/hero_shoot.wav")
 ## Default hero bullet type.
-const BULLET: PackedScene = preload("res://scenes/hero/Bullet.tscn")
+const BULLET: PackedScene = preload ("res://scenes/hero/Bullet.tscn")
 @onready var hud = $HUD
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
@@ -34,18 +34,15 @@ var is_reloading: bool = false
 
 var health: int = max_health
 
-
 class Animations:
 	const IDLE := "idle"
 	const ACCELERATING := "accelerating"
 	const CRUISING := "cruising"
 
-
 ## Shooting action to be handled by the Game Manager.
 signal shoot(bullet: PackedScene, direction: float, location: Vector2, audio_clip: AudioStream)
 signal death
 signal quit
-
 
 func _ready():
 	animated_sprite_2d.animation = Animations.IDLE
@@ -53,18 +50,15 @@ func _ready():
 	hud.update_health(health, max_health)
 	timer = Timer.new()
 	add_child(timer)
-	timer.connect(Literals.Signals.TIMEOUT, func(): can_shoot = true)
-
+	timer.connect(Literals.Signals.TIMEOUT, func(): can_shoot=true)
 
 func _process(_delta):
 	if can_shoot and ammunition != 0 and !is_reloading:
 		if Input.is_action_pressed(Literals.Inputs.SHOOT):
 			_shoot()
 
-
 func _physics_process(delta):
 	player_movement(delta)
-
 
 func _unhandled_key_input(event):
 	if event is InputEventKey:
@@ -72,7 +66,6 @@ func _unhandled_key_input(event):
 			_reload()
 		elif event.is_action_pressed(Literals.Inputs.QUIT):
 			quit.emit()
-
 
 ## Handles player movement.
 func player_movement(delta):
@@ -92,7 +85,6 @@ func player_movement(delta):
 	animated_sprite_2d.play()
 	move_and_slide()
 
-
 func _shoot():
 	shoot.emit(BULLET, rotation, position, HERO_SHOOT_SFX)
 	ammunition = ammunition - 1
@@ -103,14 +95,12 @@ func _shoot():
 	else:
 		_reload()
 
-
 func _reload():
 	is_reloading = true
 	await get_tree().create_timer(1.0, false).timeout
 	ammunition = magazine_size
 	hud.update_ammo_count(ammunition, magazine_size)
 	is_reloading = false
-
 
 ## Handles the health of the hero upon collision
 func get_hurt(damage: int):
@@ -120,7 +110,6 @@ func get_hurt(damage: int):
 		self.queue_free()
 	hud.update_health(health, max_health)
 
-
 func get_heal(heal: int):
 	health += heal
 	hud.update_health(health, max_health)
@@ -129,10 +118,8 @@ func get_heal(heal: int):
 		health -= 1
 		hud.update_health(health, max_health)
 
-
 func update_score(amount: int):
 	hud.update_score(amount)
-
 
 ## Handles normalization of player input.
 func _get_input():
