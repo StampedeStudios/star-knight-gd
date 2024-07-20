33c3264f13221e6ad0b4e1336082ac02865f4e77
simone-lungarella <simonelungarella@gmail.com>
2024-07-20 10:35:00 +0200

    fix: corrected enemy collisions and lifecycle of reward
    - Now rewards correctly spawn at enemy death position;
    - Enemy casts raycast from global_position instead of local position;
    - Modified collision shape to make it more resilient to speedy bullets collisions.

--- scripts/weapon_and_bullet/bullet.gd
+++ scripts/weapon_and_bullet/bullet.gd
@@ -39,7 +39,7 @@ func _on_area_entered(area):
 	if area.has_method("get_hurt"):
 		area.get_hurt(damage)
 
-	_remove_bullet()
+	call_deferred("_remove_bullet")
 
 
 func _randomize_bullet_sprite():
@@ -60,7 +60,7 @@ func _on_body_entered(body):
 	if body.has_method("get_hurt"):
 		body.get_hurt(damage)
 
-	_remove_bullet()
+	call_deferred("_remove_bullet")
 
 
 func _remove_bullet():
